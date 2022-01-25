08116c878024465c445a4b66cb15ed89eaeef204
Jair Partida <jairpartida1@gmail.com>
2022-01-24 21:53:27 -0700

    first commit

---
+++ capitalize.js
@@ -0,0 +1,15 @@
+//This is a Function that capitalize only the first 
+//letter of each word in a string
+
+const Capitalize = (str) => {
+
+    const sentence = str.split(" ");
+
+    for (let i = 0; i < sentence.length; i++) {
+        sentence[i] = sentence[i].charAt(0).toUpperCase() + sentence[i].slice(1).toLowerCase();
+    }
+
+    return sentence.join(" ");
+} 
+
+console.log(Capitalize('jair partida santana'));
